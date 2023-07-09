a511a5a2c08a80fa1b81779a9afd86d4bd6159bd
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-07-09 12:32:13 -0400

    exported index

--- index.js
+++ index.js
@@ -22,3 +22,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
+
+module.exports = app
